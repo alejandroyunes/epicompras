1f7ee55e24e220a325ff84f61ea813e51d02cbcd
Alejandro Yunes <alejandroyunes@outlook.com>
2023-12-17 23:56:21 -0500

    changing dropdown

--- src/app/components/ui/nav/DesktopNav/Profile/index.tsx
+++ src/app/components/ui/nav/DesktopNav/Profile/index.tsx
@@ -7,19 +7,7 @@ export default function Profile() {
     <div className={stylex(styles.container)}>
 
       <div className={stylex(styles.icon)}>
-
-        <details >
-          <summary role="button">
-            <ProfileSvg />
-          </summary>
-          <ul>
-            <li><a href="#">I'm a dropdown.</a></li>
-            <li><a href="#">In Pure CSS</a></li>
-            <li><a href="#">As in...</a></li>
-            <li><a href="#">No JavaScript.</a></li>
-            <li><a href="#">At All.</a></li>
-          </ul>
-        </details>
+        <ProfileSvg />
       </div>
     </div>
   );
