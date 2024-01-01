4de9d04dae860f1351e93d329ab5a15e4ef8dda2
Alejandro Yunes <alejandroyunes@outlook.com>
2024-01-01 15:17:41 -0500

    chaging icon sizes

--- src/app/components/ui/Assets/Icons/ModeSvg.tsx
+++ src/app/components/ui/Assets/Icons/ModeSvg.tsx
@@ -1,6 +1,6 @@
 export default function ModeSvg() {
   return (
-    <svg width="34px" height="34px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+    <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
       stroke="currentColor"
       strokeWidth="1"
       strokeLinecap="square"
